426f8dd97ead8aed23004cd2cee8b3b86c87117c
poulakise <poulakise@interamerican.gr>
2024-11-09 07:02:39 +0200

    feat(App): duration Usage.

--- src/main/java/com/evansp/App.java
+++ src/main/java/com/evansp/App.java
@@ -1,13 +1,27 @@
 package com.evansp;
 
+import java.time.Duration;
+
 /**
- * Hello world!
- *
+ * Main.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    /**
+     * Main
+     * @param args args
+     */
+    public static void main( String[] args)  {
+        durationManagement();
+    }
+
+    /**
+     * sample usage of {@link Duration}.
+     */
+    private static void durationManagement() {
+        Duration duration = Duration.ofDays(1);
+
+        System.out.println(duration); // PT24H
+        System.out.println(duration.plus(Duration.ofDays(1))); // PT48H
+        System.out.println(duration); // PT24H
     }
 }
