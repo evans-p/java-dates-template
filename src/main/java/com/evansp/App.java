a112da3379f8c2be2b7af3c9bef628962f6cb71f
poulakise <poulakise@interamerican.gr>
2024-11-09 07:22:34 +0200

    feat(App): Period Usage.

--- src/main/java/com/evansp/App.java
+++ src/main/java/com/evansp/App.java
@@ -1,6 +1,7 @@
 package com.evansp;
 
 import java.time.Duration;
+import java.time.Period;
 
 /**
  * Main.
@@ -12,6 +13,7 @@ public class App {
      */
     public static void main( String[] args)  {
         durationManagement();
+        periodManagement();
     }
 
     /**
@@ -23,5 +25,16 @@ public class App {
         System.out.println(duration); // PT24H
         System.out.println(duration.plus(Duration.ofDays(1))); // PT48H
         System.out.println(duration); // PT24H
+    }
+
+    /**
+     * sample usage of {@link Period}.
+     */
+    private static void periodManagement() {
+        Period period = Period.ofYears(1);
+
+        System.out.println(period); // P1Y
+        System.out.println(period.plusMonths(2)); // P1Y2M
+        System.out.println(period); // P1Y
     }
 }
