4e1da96e639a77dc92307dc2ed1ec93cbecc2d41
evans-p <evanspoulaki@gmail.com>
2024-11-11 01:22:52 +0200

    feat(App): flightLandingDateTimeManagement

--- src/main/java/com/evansp/App.java
+++ src/main/java/com/evansp/App.java
@@ -1,24 +1,30 @@
 package com.evansp;
 
+import java.time.Clock;
 import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.OffsetDateTime;
 import java.time.Period;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Main.
  */
+@SuppressWarnings("unused")
 public class App {
     /**
      * Main
      * @param args args
      */
     public static void main( String[] args)  {
-        workPeriodsManagement();
+        flightLandingDateTimeManagement();
     }
 
     private static void workPeriodsManagement() {
@@ -64,5 +70,39 @@ public class App {
         System.out.println("France: " + franceDateTime.toLocalDateTime());
         System.out.println("Greece: " + greeceDateTime.toLocalDateTime());
         System.out.println("England: " + englandDateTime.toLocalDateTime());
+    }
+
+    private static void instantManagement() {
+        Instant instant = Instant.now();
+        Instant greeceInstant = Instant.now(Clock.system(ZoneId.of("UTC+2")));
+
+        System.out.println(greeceInstant);
+        System.out.println(instant);
+        System.out.println(instant.atZone(ZoneId.of("UTC+2")));
+    }
+
+
+    private static void flightLandingDateTimeManagement() {
+        ZoneOffset originZone = ZoneOffset.of("+0");
+        ZoneOffset destinationZone = ZoneOffset.of("+8");
+
+        // DateTimes for local.
+        LocalDateTime departure = LocalDateTime.now(originZone);
+        LocalDateTime arrival = departure.plusHours(17L).plusMinutes(55L);
+
+        System.out.println("Departure LocalDateTime: " + departure);
+        System.out.println("Arrival LocalDateTime: " + arrival);
+
+        OffsetDateTime departureOriginZone = OffsetDateTime.of(departure, originZone);
+        OffsetDateTime departureDestinationZone = departureOriginZone.withOffsetSameInstant(destinationZone);
+
+        System.out.println("departureOriginZone: " + departureOriginZone);
+        System.out.println("departureDestinationZone: " + departureDestinationZone);
+
+        OffsetDateTime arrivalOriginZone = OffsetDateTime.of(arrival, originZone);
+        OffsetDateTime arrivalDestinationZone = arrivalOriginZone.withOffsetSameInstant(destinationZone);
+
+        System.out.println("arrivalOriginZone: " + arrivalOriginZone);
+        System.out.println("arrivalDestinationZone: " + arrivalDestinationZone);
     }
 }
